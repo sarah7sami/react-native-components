798af53e8f78094f0cc39938197808319163799a
Sarah Sami <sarah@Sarahs-MacBook-Pro.local>
2022-12-06 20:01:12 -0500

    sample list with name and description - filtered by both via search bar component

---
+++ screens/Home.js
@@ -0,0 +1,69 @@
+// Home.js
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+
+import SampleList from "../components/SampleList";
+import SearchBar from "../components/SearchBar";
+
+const Home = () => {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [fakeData, setFakeData] = useState();
+
+  // get data from the fake api
+  useEffect(() => {
+    const getData = async () => {
+      const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+      );
+      const data = await apiResponse.json();
+      setFakeData(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.root}>
+      {!clicked && <Text style={styles.title}>Programming Languages</Text>}
+
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+      {!fakeData ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        
+          <SampleList
+            searchPhrase={searchPhrase}
+            data={fakeData}
+            setClicked={setClicked}
+          />
+        
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: "100%",
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+    marginLeft: "10%",
+  },
+});
